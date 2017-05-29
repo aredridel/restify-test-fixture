aa11e79430e3433c2c5528598ee8295dfe5b2621
Aria Stewart <aredridel@nbtsc.org>
2017-05-29 14:09:28 -0400

    Hack around restify ueing self-assigned IPs in urls

--- index.async.js
+++ index.async.js
@@ -1,5 +1,6 @@
 const promisify = require('es6-promisify');
 const restify = require('restify');
+const url = require('url');
 
 module.exports = async function restifyFixture(fn) {
 	const server = restify.createServer()
@@ -7,10 +8,14 @@ module.exports = async function restifyFixture(fn) {
 	const listen = promisify(server.listen, server);
 	const close = promisify(server.close, server);
 	await listen(0);
+
+	const u = url.parse(server.url);
+	delete u.host;
+	u.hostname = 'localhost'
 	return {
 		async done() {
 		 	await close();
 		},
-		url: server.url
+		url: url.format(u)
 	};
 }
